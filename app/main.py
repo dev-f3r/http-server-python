3a1522ea4ce6a2b051f7316c0a0f46f8d33930ce
Fernando Castro <devfercastro@gmail.com>
2024-04-14 17:09:42 -0300

    Make server capable of handle multiple request

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 # Uncomment this to pass the first stage
 import socket
 import re
-
+import asyncio
+from asyncio import StreamReader, StreamWriter
 
 RESPONSE_HEADERS = {
     200: "HTTP/1.1 200 OK",
     404: "HTTP/1.1 404 Not Found",
 }
-
 CONTENT_TYPE = "Content-Type: text/plain"
+PORT = 4221  # ? Port number
 
 
 class HTTPRequestParser:
@@ -35,8 +36,14 @@ class HTTPRequestParser:
             self._lines = request_data.split("\r\n")
 
             self._method, self._path, self._http_version = self._lines[0].split(" ")
-            self._host = self._lines[1].split(" ")[1]
-            self._user_agent = self._get_agent(self._lines[1:])
+
+            # If the are more header that the first
+            if self._lines[1] != "":
+                self._host = self._lines[1].split(" ")[1]
+                self._user_agent = self._get_agent(self._lines[1:])
+            else:
+                self._host = ""
+                self._user_agent = ""
 
         except (UnicodeDecodeError, IndexError, ValueError) as e:
             raise ValueError("Invalid HTTP request data") from e
@@ -120,77 +127,73 @@ def build_response(lines: list[str], body, close_connection=True):
     return response
 
 
-def main():
-    PORT = 4221  # ? Port number
+async def handler(reader: StreamReader, writer: StreamWriter):
+    data = await reader.read(1024)
+    request_data = HTTPRequestParser(data)  # Read data, up to 1024B
+
+    client_address = writer.get_extra_info("peername")  # Get the client address
+
+    path = request_data.path  # Request's path
+    method = request_data.method  # Request's method
+
+    response = ""  # The response for the current request
+
+    # ? Build the reponse based on the following cases
+    match (method):
+        case "GET":
+            # If is just the path "/"
+            if path == "/":
+                response = build_status_line(200, True)
+            # If the path contains the "echo" word
+            elif "echo" in path:
+                path_string = extract_string(path)  # The random string after "/echo/"
+                response = build_response(
+                    [
+                        build_status_line(200),  # Response status
+                        CONTENT_TYPE,  # Content type header
+                    ],
+                    path_string,  # Body
+                )
+            # If the path contains the "user-agent" word
+            elif "user-agent" in path:
+                user_agent = request_data._user_agent
+                response = build_response(
+                    [
+                        build_status_line(200),
+                        CONTENT_TYPE,
+                    ],
+                    user_agent,  # The request's user-agent as the body
+                )
+            # If the requested path is different from "/" returns 404
+            else:
+                response = build_status_line(404, True)
+        case _:
+            print(f"Unsupported method: {request_data.method}")
+
+    # ? Prints the info about the requets
+    print("Connection info:")
+    print(f"\tAddress: {client_address}")
+    print(f"\tData:")
+    for line in request_data.lines:
+        print(f"\t-{line}")  # Print each line of the request.
+    print(f"\tResponse: \n{response}")
+
+    # ? Send the response
+    writer.write(response.encode()) # Buffered the response
+    await writer.drain() # Send the response
+    writer.close() # Close connection
+
+
+async def main():
+    server = await asyncio.start_server(handler, "localhost", PORT)  # Create the server
 
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
+    print(f"Listening connections on port {PORT}")
 
-    # Uncomment this to pass the first stage
-    #
-    server_socket = socket.create_server(("localhost", PORT), reuse_port=True)
-
-    print(f"Server started. Waiting for connections on port {PORT}")
-
-    while True:
-        client_socket, client_address = server_socket.accept()  # wait for client
-
-        request_data = HTTPRequestParser(
-            client_socket.recv(1024)
-        )  # Read data, up to 1024B
-
-        path = request_data.path  # Request's path
-        method = request_data.method  # Request's method
-
-        response = ""  # The response for the current request
-
-        # ? Build the reponse based on the following cases
-        match (method):
-            case "GET":
-                # If is just the path "/"
-                if path == "/":
-                    response = build_status_line(200, True)
-                # If the path contains the "echo" word
-                elif "echo" in path:
-                    path_string = extract_string(
-                        path
-                    )  # The random string after "/echo/"
-                    response = build_response(
-                        [
-                            build_status_line(200),  # Response status
-                            CONTENT_TYPE,  # Content type header
-                        ],
-                        path_string,  # Body
-                    )
-                # If the path contains the "user-agent" word
-                elif "user-agent" in path:
-                    user_agent = request_data._user_agent
-                    response = build_response(
-                        [
-                            build_status_line(200),
-                            CONTENT_TYPE,
-                        ],
-                        user_agent,  # The request's user-agent as the body
-                    )
-                # If the requested path is different from "/" returns 404
-                else:
-                    response = build_status_line(404, True)
-            case _:
-                print(f"Unsupported method: {request_data.method}")
-
-        # ? Prints the info about the requets
-        print("Connection info:")
-        print(f"\tAddress: {client_address}")
-        print(f"\tData:")
-        for line in request_data.lines:
-            print(f"\t-{line}")  # Print each line of the request.
-        print(f"\tResponse: \n{response}")
-
-        client_socket.sendall(response.encode())  # Send the response
-
-        client_socket.close()  # Close the connection
-        # server_socket.close() # Shutdown server
+    async with server:
+        await server.serve_forever() # Make listen multiple petitions
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main()) # Run main coroutine with asyncio
